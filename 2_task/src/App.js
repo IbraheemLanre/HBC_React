03d7c583bab4c34f45a1af81cab9fd3df790390d
IbraheemLanre <s2001108@edu.bc.fi>
2020-10-28 15:38:22 +0200

    added different images to Animal component

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 import Animal from "./component/Animal";
+import lion from "./component/Animal/Lion.jpg";
+import dog from "./component/Animal/dog.jpg"
+import cat from "./component/Animal/cat.jpg"
 
 function Welcome() {
   return <h1>Hello World</h1>;
@@ -10,9 +13,9 @@ function App() {
   return (
     <div className="App">
       <Welcome />
-      <Animal name="Lion" />
-      <Animal name="Lion" />
-      <Animal name="Lion" />
+      <Animal name="Lion" image={lion} />
+      <Animal name="Lion" image={dog}/>
+      <Animal name="Lion" image={cat}/>
     </div>
   );
 }
--- 2_task/src/component/Animal.js
+++ 2_task/src/component/Animal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Animal.css";
-import image from "./Animal/Lion.jpg";
+// import image from "./Animal/Lion.jpg";
 
 function Animal(props) {
   const popup = () => {
@@ -11,7 +11,7 @@ function Animal(props) {
     <div className="card">
       <h1>{props.name}</h1>
       <div className="image">
-        <img src={image} alt="lion" />
+        <img src={props.image} alt="lion" />
       </div>
       <button onClick={popup}>Click</button>
     </div>
