42dd95ca063fb0bd96d307e5b78e6656f5719479
IbraheemLanre <s2001108@edu.bc.fi>
2020-11-01 00:17:45 +0200

    added event handlers

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -6,10 +6,36 @@ class App extends Component {
     likes: 0,
   };
 
+  addLikes = () => {
+    console.log("Added like");
+    this.setState({
+      likes: this.state.likes + 1,
+    });
+  };
+
+  removeLikes = (like) => {
+    console.log("Removed like");
+    if (this.state.likes > 0) {
+      this.setState({
+        likes: this.state.likes - 1,
+      });
+    }
+  };
+
+  resetLikes = () => {
+    console.log("Reset like");
+    this.setState({
+      likes: 0,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Total like: {this.state.likes}</h1>
+        <button onClick={this.addLikes}>Add like</button>
+        <button onClick={this.removeLikes}>Remove like</button>
+        <button onClick={this.resetLikes}>Reset like</button>
       </div>
     );
   }
